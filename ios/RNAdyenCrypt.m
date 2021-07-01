ad8f09e3600d98f28273aaf22f5a07abc96c0c48
Raphael Novaes dos Santos <raphaelnovaesdossantos@MacBook-Pro-de-Raphael.local>
2021-07-01 18:14:46 -0300

    Inicio de projeto

--- ios/RNAdyenCrypt.m
+++ ios/RNAdyenCrypt.m
@@ -1,6 +1,7 @@
 
 #import "RNAdyenCrypt.h"
-#import "Adyen"
+#import "AdyenCard/AdyenCard.h"
+
 @implementation RNAdyenCrypt
 
 - (dispatch_queue_t)methodQueue
@@ -9,35 +10,46 @@
 }
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(encrypt:(NSString *)cardHolderNameValue number:(NSString *)cardNumberValue cvc:(NSString *)cvc expiryMonth:(NSString *)expiryMonth expiryYear:(NSString *)expiryYear publickey:(NSString *)publicKey resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_METHOD(encrypt:
+                  (NSString *)holderName
+                  number:(NSString *)number
+                  cvc:(NSString *)cvc
+                  expiryMonth:(NSString *)expiryMonth
+                  expiryYear:(NSString *)expiryYear
+                  publickey:(NSString *)publicKey
+                  resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-    let cardValidator = CardNumberValidator()
-        let isCardValid: Bool = cardValidator.isValid(cardNumberValue ?? "")
-        let expiryValidator = CardExpiryDateValidator()
-        let isExpiryValid: Bool = expiryValidator.isValid(expiryDate)
-        let securityCodeValidator = CardSecurityCodeValidator()
-        let isSecurityCodeValid = securityCodeValidator.isValid(securityCodeValue)
-        if isCardValid && isExpiryValid && isSecurityCodeValid && (cardHolderNameValue != nil){
-            let cardObject = CardEncryptor.Card(number: cardNumberValue, securityCode: securityCodeValue, expiryMonth: expiryMonthValue, expiryYear: expiryYearValue)
-            let encryptedCard = CardEncryptor.encryptedCard(for: cardObject, publicKey: self.PUBLIC_KEY)
-            makePayment(details: encryptedCard, holderName: cardHolderNameValue ?? "", type: "scheme")
-        }
-    // @try {
-    //     ADYCard *card = [ADYCard new];
-    //     card.generationtime = [NSDate new];
-    //     card.number = number;
-    //     card.holderName = holderName;
-    //     card.cvc = cvc;
-    //     card.expiryMonth = expiryMonth;
-    //     card.expiryYear = expiryYear;
-        
-    //     NSData *cardData = [card encode];
-    //     NSString *encryptedCard = [ADYEncrypter encrypt:cardData publicKeyInHex:publicKey];
-    //     resolve(encryptedCard);
-    // } @catch(NSException *exception) {
-    //     NSError *error = nil;
-    //     reject(@"adyen_error", @"failed to encrypt", error);
-    // }
+    
+    
+     @try {
+//         CardNumberValidator *cardValidador = [CardNumberValidator new];
+//         NSObject *cardValidator = ;
+//         CardNumberValidator *card = [CardNumberValidator new];
+//         card.generationtime = [NSDate new];
+//         card.number = cardNumberCard;
+//         card.holderName = cardHolderName;
+//         card.cvc = cvc;
+//         card.expiryMonth = expiryMonth;
+//         card.expiryYear = expiryYear;
+//
+//         NSData *cardData = [card encode];
+//         NSString *encryptedCard = [ADYEncrypter encrypt:cardData publicKeyInHex:publicKey];
+//         resolve(encryptedCard);
+     } @catch(NSException *exception) {
+         NSError *error = nil;
+         reject(@"adyen_error", @"failed to encrypt", error);
+     }
+//    let cardValidator = CardNumberValidator()
+//        let isCardValid: Bool = cardValidator.isValid(cardNumberValue ?? "")
+//        let expiryValidator = CardExpiryDateValidator()
+//        let isExpiryValid: Bool = expiryValidator.isValid(expiryDate)
+//        let securityCodeValidator = CardSecurityCodeValidator()
+//        let isSecurityCodeValid = securityCodeValidator.isValid(securityCodeValue)
+//        if isCardValid && isExpiryValid && isSecurityCodeValid && (cardHolderNameValue != nil){
+//            let cardObject = CardEncryptor.Card(number: cardNumberValue, securityCode: securityCodeValue, expiryMonth: expiryMonthValue, expiryYear: expiryYearValue)
+//            let encryptedCard = CardEncryptor.encryptedCard(for: cardObject, publicKey: self.PUBLIC_KEY)
+//            makePayment(details: encryptedCard, holderName: cardHolderNameValue ?? "", type: "scheme")
+//        }
 }
 
 
--- ios/RNAdyenCrypt.podspec
+++ ios/RNAdyenCrypt.podspec
@@ -1,29 +1,19 @@
-require 'json'
-
-package = JSON.parse(File.read(File.join(__dir__, 'package.json')))
-
 Pod::Spec.new do |s|
   s.name         = "RNAdyenCrypt"
-  s.version      = "1.0.1"
+  s.version      = "1.0.0"
   s.summary      = "RNAdyenCrypt"
   s.description  = <<-DESC
-                  RNAdyenCrypt
+                    RNAdyenCrypt
                    DESC
-  s.homepage     = ""
+  s.homepage     = "https://github.com/rph17/RNAdyenCrypt"
   s.license      = "MIT"
-  # s.license      = { :type => "MIT", :file => "FILE_LICENSE" }
   s.author             = { "author" => "rph7@live.com" }
   s.platform     = :ios, "7.0"
-  s.source       = { :git => "https://github.com/author/RNAdyenCrypt.git", :tag => "master" }
-  # s.source_files  = "RNAdyenCrypt/**/*.{h,m}"
-  s.source_files = "ios/**/*.{h,m,swift}"
+  s.source       = { :git => "https://github.com/rph17/RNAdyenCrypt.git", :tag => "master" }
+  s.source_files  = "*.{h,m}"
   s.requires_arc = true
 
-
-  s.dependency "React"
-  s.dependency "Adyen"
-  #s.dependency "others"
-
+  s.dependency 'React'
+  s.dependency "Adyen/Card"
+  s.dependency "Adyen/Encryption"
 end
-
-  
